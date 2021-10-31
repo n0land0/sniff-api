f5baf0cb06ba10f5001857a783bdb45ae4c18ca3
Nolan Caine <nolancaine@gmail.com>
2021-10-31 17:29:29 -0600

    Create Procfile

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ app.use(bodyParser.json())
 const cors = require('cors')
 app.use(cors())
 
+const port = 'https://sniff-api.herokuapp.com'
+
 app.locals.title = 'Sniff'
 
 app.set('port', process.env.PORT || 3001)
 
 app.listen(app.get('port'), () => {
-    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
-  })
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+})
+
+app.get('/', (request, reponse) => {
+  console.log('working')
+  // response.json({
+  //   test: 'test'
+  // })
+})
