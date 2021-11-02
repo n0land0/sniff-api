c0d5e10ba07ca102b17ccea614f7cef2d26bb22e
Nolan Caine <nolancaine@gmail.com>
2021-11-01 20:17:51 -0600

    Construct empty users table

---
+++ database/knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
+  // staging: {
+  development: {
+    client: 'postgresql',
+    // connection: {
+    //   database: 'sniffdb',
+    //   user:     'nolancaine',
+    //   password: null,
+    // },
+    connection: 'postgres://localhost/sniffdb',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
---
+++ database/migrations/20211101213705_init.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('owner_name');
+      table.string('dog_name');
+      table.string('profile_pic');
+      table.string('dog_breed');
+      table.string('bio');
+      table.json('appointments');
+
+      table.timestamps(true, true);
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('users')
+};
