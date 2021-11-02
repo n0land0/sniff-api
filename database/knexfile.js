1f16e8347c39b162f24a99a5bcfee417507814f0
Nolan Caine <nolancaine@gmail.com>
2021-11-02 16:09:42 -0600

    Add production env

--- database/knexfile.js
+++ database/knexfile.js
@@ -17,6 +17,29 @@ module.exports = {
     //   user:     'xlafidslvqdrzg',
     //   password: 'aa0fbed94304d1061495b77612c28c23de599ef58059c95b479c2ddc54f48538',
     // },
+    connection: 'postgres://localhost/sniffdb',
+    // connection: 'postgres://xlafidslvqdrzg:aa0fbed94304d1061495b77612c28c23de599ef58059c95b479c2ddc54f48538@ec2-54-145-188-92.compute-1.amazonaws.com:5432/d65h0seeuo96k7',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'postgresql',
+    // connection: {
+    //   database: 'd65h0seeuo96k7',
+    //   user:     'xlafidslvqdrzg',
+    //   password: 'aa0fbed94304d1061495b77612c28c23de599ef58059c95b479c2ddc54f48538',
+    // },
     // connection: 'postgres://localhost/sniffdb',
     connection: 'postgres://xlafidslvqdrzg:aa0fbed94304d1061495b77612c28c23de599ef58059c95b479c2ddc54f48538@ec2-54-145-188-92.compute-1.amazonaws.com:5432/d65h0seeuo96k7',
     pool: {
@@ -32,21 +55,5 @@ module.exports = {
     },
     useNullAsDefault: true
   },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
