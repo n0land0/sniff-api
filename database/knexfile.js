5d3a4ed09062928b5dd0c885fc7b92e8d3ddfcc9
Nolan Caine <nolancaine@gmail.com>
2021-11-02 19:17:43 -0600

    Add ssl param

--- database/knexfile.js
+++ database/knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
       database: 'd65h0seeuo96k7',
       user:     'xlafidslvqdrzg',
       password: 'aa0fbed94304d1061495b77612c28c23de599ef58059c95b479c2ddc54f48538',
-      ssl: true
+      ssl: { rejectUnauthorized: false }
     },
     // connection: 'postgres://xlafidslvqdrzg:aa0fbed94304d1061495b77612c28c23de599ef58059c95b479c2ddc54f48538@ec2-54-145-188-92.compute-1.amazonaws.com:5432/d65h0seeuo96k7',
     pool: {
