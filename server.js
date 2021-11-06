3e8993ab41b6483b3ca7e88e00c05045bb4b7506
Joshua Benefield <benefield.joshua@gmail.com>
2021-11-06 16:18:47 -0600

    remove parsing id to integer from post endpoint

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/api/v1/users/:userId', (request, response) => {
 app.post('/api/v1/appointments', (request, response) => {
   const playdate = request.body
   sniffDB('appointments').insert({
-      id: +playdate.id,
+      id: playdate.id,
       owner_ids: playdate.owner_ids,
       location: playdate.location,
       date: playdate.date,
