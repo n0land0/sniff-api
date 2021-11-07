932be2c43eb5dd2319fdd2b928bacaeaedad757a
Joshua Benefield <benefield.joshua@gmail.com>
2021-11-06 19:26:27 -0600

    why am i still trying

--- server.js
+++ server.js
@@ -73,9 +73,9 @@ app.post('/api/v1/appointments', (request, response) => {
   sniffDB('appointments')
     .insert({
       id: playdate.id,
-      owner_ids: JSON.stringify(playdate.owner_ids),
-      dog_park: JSON.stringify(playdate.location),
-      date: JSON.stringify(playdate.date),
+      owner_ids: `${playdate.owner_ids}`,
+      dog_park: `${playdate.location}`,
+      date: `${playdate.date}`,
     })
 })
 
