d82bbf0ffbfaaaff7d01020b646370177b77b98c
Nolan Caine <nolancaine@gmail.com>
2021-11-08 12:52:16 -0700

    Create appointments data

---
+++ appointmentsData.js
@@ -0,0 +1,66 @@
+const lorem = 'lol hey im a dog'
+
+const userData = [
+    {
+        "date": "2021-12-24",
+        "dog_park": "Kennedy Dog Park",
+        "owners": [3, 2]
+    },
+    {
+        "date": "2021-12-17",
+        "dog_park": "Barnum Dog Park",
+        "owners": [5, 2]
+    },
+    {
+        "date": "2021-12-10",
+        "dog_park": "Lowry Dog Park",
+        "owners": [7, 2]
+    },
+    {
+        "date": "2021-12-03",
+        "dog_park": "Berkeley Dog Park",
+        "owners": [9, 2]
+    },
+    {
+        "date": "2021-11-24",
+        "dog_park": "Midtown Dog Park",
+        "owners": [11, 2]
+    },
+    {
+        "date": "2021-11-17",
+        "dog_park": "Carla Madison Dog Park",
+        "owners": [13, 2]
+    },
+    {
+        "date": "2021-11-10",
+        "dog_park": "Railyard Dog Park",
+        "owners": [1, 2]
+    },
+    {
+        "date": "2021-11-03",
+        "dog_park": "Fuller Dog Park",
+        "owners": [4, 2]
+    },
+    {
+        "date": "2021-10-24",
+        "dog_park": "Greenway Off-Leash Dog Park",
+        "owners": [6, 2]
+    },
+    {
+        "date": "2021-10-17",
+        "dog_park": "Skyline Off-Leash Area",
+        "owners": [8, 2]
+    },
+    {
+        "date": "2021-10-10",
+        "dog_park": "Lawson Dog Park",
+        "owners": [10, 2]
+    },
+    {
+        "date": "2021-10-03",
+        "dog_park": "Little Boxcar Dog Park",
+        "owners": [12, 2]
+    },
+]
+
+module.exports = userData
---
+++ database/seeds/appointments.js
@@ -0,0 +1,9 @@
+const appointmentsData = require('../../appointmentsData.js')
+
+exports.seed = function(knex) {
+  return knex('appointments').del()
+    .then(() => {
+      return knex('appointments').insert(appointmentsData)
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`))
+};
