bce1fbeeed236bbc91c07cba66996775924def2e
Nolan Caine <nolancaine@gmail.com>
2021-11-08 10:10:20 -0700

    Add more dummy users

--- userData.js
+++ userData.js
@@ -8,14 +8,14 @@ const userData = [
     owner_name: 'Nolan',
     dog_name:  'Biscuit',
     profile_pic: 'https://i.imgur.com/i4UwYMu.jpeg',
-    dog_breed: 'Sheppard',
+    dog_breed: 'Shepherd',
     bio: lorem,
     appointments: []
   },
   {
     id: 2,
     owner_name: 'Joshua',
-    dog_name: 'Fido',
+    dog_name: 'Bruce',
     profile_pic: 'https://i.imgur.com/LRoLTlK.jpeg',
     dog_breed: 'Labrador',
     bio: lorem,
@@ -224,7 +224,52 @@ const userData = [
     dog_breed: 'Corgi',
     bio: lorem,
     appointments: []
-  }
+  },
+  {
+    id: 6,
+    owner_name: 'Stacy',
+    dog_name: 'Herman',
+    profile_pic: 'https://149366112.v2.pressablecdn.com/wp-content/uploads/2020/08/shutterstock_1359893087-scaled.jpg',
+    dog_breed: 'German',
+    bio: lorem,
+    appointments: []
+  },
+  {
+    id: 7,
+    owner_name: 'Ricardo',
+    dog_name: 'Cinnamon',
+    profile_pic: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2018/09/14104433/Golden-Retriever-licking-the-face-of-a-man-outdoors.jpg',
+    dog_breed: 'Golden Retriever',
+    bio: lorem,
+    appointments: []
+  },
+  {
+    id: 8,
+    owner_name: 'Hilde',
+    dog_name: 'Xerxes',
+    profile_pic: 'https://i.insider.com/5b8ff6023cccd14f008b458c?width=750&format=jpeg&auto=webp',
+    dog_breed: 'Spaniel',
+    bio: lorem,
+    appointments: []
+  },
+  {
+    id: 9,
+    owner_name: 'Yua',
+    dog_name: 'Mabel',
+    profile_pic: 'https://images2.minutemediacdn.com/image/upload/c_crop,h_844,w_1500,x_0,y_40/f_auto,q_auto,w_1100/v1555004118/shape/mentalfloss/51153-istock-485392746.jpg',
+    dog_breed: 'Shepherd',
+    bio: lorem,
+    appointments: []
+  },
+  {
+    id: 10,
+    owner_name: 'Eryn',
+    dog_name: 'Scoops',
+    profile_pic: 'https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/06/GettyImages-1094894550_header-1024x575.jpg?w=1155&h=1528',
+    dog_breed: 'Bulldog',
+    bio: lorem,
+    appointments: []
+  },
 ]
 
 module.exports = userData
